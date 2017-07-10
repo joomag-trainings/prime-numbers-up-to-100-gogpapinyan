7b4f33d5ad5ae5c96292490cc254f0380190ed48
Gohar <goharpapinyan@gmail.com>
2017-07-10 23:17:45 +0400

    Prime numbers up to 100

--- primeNumbers.php
+++ primeNumbers.php
@@ -1,16 +1,15 @@
 <?php
-    $Primes = [];
-    for ($i = 2; $i <= 100; ++$i){
-        for ($j = 2; $j <= $i/2; $j++){
-            if ($i%$j == 0) {
+$Primes = [];
+for ($i = 2; $i <= 100; ++$i) {
+
+    for ($j = 2; $j <= $i/2; $j++) {
+        if ($i%$j == 0) {
                 break;
-            }
-        }
-        if ($j > $i/2) {
-            array_push($Primes, $i);
-            echo "$i ";
         }
     }
 
-
-
+    if ($j > $i/2) {
+        array_push($Primes, $i);
+        echo "$i ";
+    }
+}
